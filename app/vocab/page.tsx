48f5c58b1735cf21072181eec4ea0ca307e18b80
mahadidev7 <mahadidev7@gmail.com>
2026-04-24 16:22:48 +0600

    21

--- app/vocab/page.tsx
+++ app/vocab/page.tsx
@@ -2,19 +2,7 @@
 // "use client";
 // import { useRouter } from "next/router";
 
-
-
-export default function vocab({ params  }) {
-
-// const router = useRouter();
-  // const { lesson } = router.query;
-
-  console.log('====================router.query================');
-  console.log(params);
-  console.log('====================================');
-
-
-
+export default function vocab() {
   return (
     <div>
       <h1 className="text-2xl font-bold mb-4">{"lesson"}</h1>

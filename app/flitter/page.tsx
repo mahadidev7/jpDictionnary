2b3691e22459c16c6d15bb18cebad652427f43b9
mahadidev7 <mahadidev7@gmail.com>
2026-05-09 12:33:04 +0600

    check feture addded

--- app/flitter/page.tsx
+++ app/flitter/page.tsx
@@ -14,6 +14,8 @@ export default function Flitter() {
   const [lessonVocab, setLessonVocab] = useState<VocabItem[]>([]);
   const [sliderActiveVocab, setSliderActiveVocab] = useState(0);
   const [isBangla, setIsBangla] = useState(true);
+  const [isShowCheckJapanese, setIsShowCheckJapanese] = useState(false);
+  const [typeKey, setTypeKey] = useState("");
 
   const handleClick = (data: VocabItem[]) => {
     setLessonVocab(data);
@@ -38,9 +40,23 @@ export default function Flitter() {
     window.open(url, "_blank", "width=500,height=700");
   };
 
+  const handelCheck = () => {
+    setIsShowCheckJapanese(true);
+    if (
+      typeKey.trim().toLowerCase() ===
+      lessonVocab[sliderActiveVocab]?.japanese.toLowerCase()
+    ) {
+      alert("Correct!");
+      return;
+    } else {
+      alert("Incorrect! Try again.");
+      return;
+    }
+  };
+
   return (
     <>
-      <div className="flex flex-wrap w-full p-2 gap-2">
+      <div className="flex overflow-auto w-full p-2 gap-2">
         {jpN5?.map((word, index) => (
           <div key={index} className="">
             <button
@@ -52,8 +68,8 @@ export default function Flitter() {
           </div>
         ))}
       </div>
-      <div className="flex flex-wrap w-full p-2 gap-2">
-        <div className="flex flex-wrap gap-2 justify-end items-center">
+      <div className="flex w-full p-2 gap-2">
+        <div className="flex overflow-auto  gap-2 items-center">
           <button
             className="bg-black p-2 text-white rounded"
             onClick={() =>
@@ -94,11 +110,77 @@ export default function Flitter() {
             className="bg-black p-2 text-white rounded"
             onClick={() => setIsBangla(!isBangla)}
           >
-            Bangla / Japanese
+            Bangla-Japanese
           </button>
         </div>
       </div>
-      <div className="flex flex-wrap justify-normal items-center gap-2 p-4">
+
+      <div className="mt-14">
+        <div className="flex justify-center items-center p-2">
+          <div className="border border-gray-300 rounded p-2  md:w-2/3">
+            <div className="mb-2 flex flex-col justify-center items-center">
+              <div className="flex flex-row gap-2 justify-end items-end w-full px-4">
+                <button
+                  className="bg-[#333] text-white font-bold p-1 rounded"
+                  onClick={() => handelCheck()}
+                >
+                  Check
+                </button>
+              </div>
+              <input
+                type="text"
+                value={typeKey}
+                onChange={(e) => setTypeKey(e.target.value)}
+                className="border border-gray-300 rounded p-1 w-full m-2 text-[#000000]"
+              />
+              {isShowCheckJapanese && (
+                <p
+                  className={`px-1 text-[18px] text-[#000000] text-start w-full ${typeKey.trim().toLowerCase() === lessonVocab[sliderActiveVocab]?.japanese.toLowerCase() ? "text-green-500" : "text-red-500"}`}
+                >
+                  {lessonVocab[sliderActiveVocab]?.japanese}
+                </p>
+              )}
+            </div>
+
+            <div className="">
+              <p className="text-lg text-[#000000]">
+                ({lessonVocab[sliderActiveVocab]?.english || "---"})
+              </p>
+              <p className="text-[14px] text-[#000000]">
+                {lessonVocab[sliderActiveVocab]?.bangle || "---"}
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-wrap justify-center items-center w-full p-2 mb-9 gap-3">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            onClick={() => {
+              setSliderActiveVocab((prev) => Math.max(0, prev - 1));
+              setTypeKey("");
+              setIsShowCheckJapanese(false);
+            }}
+          >
+            Previous
+          </button>
+          <p className="text-center text-[16px] text-[#000000]">
+            {sliderActiveVocab + 1} / {lessonVocab.length}
+          </p>
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            onClick={() => {
+              setSliderActiveVocab((prev) =>
+                Math.min(lessonVocab.length - 1, prev + 1),
+              );
+              setTypeKey("");
+              setIsShowCheckJapanese(false);
+            }}
+          >
+            Next
+          </button>
+        </div>
+      </div>
+      <div className="flex flex-wrap justify-normal items-center gap-2 p-4 mt-65">
         {lessonVocab?.map((vocab, index) => (
           <div
             key={index}
@@ -107,7 +189,9 @@ export default function Flitter() {
             }}
             className="flex justify-between items-center"
           >
-            <div className={` ${sliderActiveVocab === index ? "bg-gray-300 px-1 rounded" : ""} cursor-pointer`}>
+            <div
+              className={` ${sliderActiveVocab === index ? "bg-gray-300 px-1 rounded" : ""} cursor-pointer`}
+            >
               {isBangla ? (
                 <p className="text-[22px] font-bold text-[#000000]">
                   {vocab?.japanese},
